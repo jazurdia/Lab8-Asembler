64b64e448f7fadc623ca717d2879a9f4604bbeb2
Javier Alejandro Azurdia Arrecis <aleazurdia1@gmail.com>
2022-05-10 02:11:40 -0600

    comparadorv2_1 creado en archivo separado

---
+++ Parte 1/comparadorv2_1.s
@@ -0,0 +1,33 @@
+/***
+* Organización de Computadoras y Assembler 
+* Universidad Del Valle de Guatemala
+* Autores: Alejandro Azurdia y Daniel Cabrera
+* Carnet: 21242 y 20289
+* Fecha: 10 de Mayo de 2022
+***/
+
+
+.text
+.align 2
+.global comparadorv2_1
+
+comparadorv2_1:
+
+    @@ Suponemos que:
+    @@ r0 = cadena de texto
+    @@ r1 = letras de la cadena de texto
+    
+
+    #Iniciamos el programa
+    ldr r1, =r0
+    ldr r1, [r1]
+
+    add r3, r3, #1 @@ contador de letras
+
+    ldrb r2, [r1, #4]
+    cmp r2, #0x00
+    bne comparadorv2_2
+
+    str r3, r0
+
+    mov pc, lr
